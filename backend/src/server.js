0e06d3dd5a81c93fea4ac1f8376d641319ba0ca1
shayan <shayan8839@gmail.com>
2026-03-14 00:17:06 +0530

    'profile image error fixed'

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5001;
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
